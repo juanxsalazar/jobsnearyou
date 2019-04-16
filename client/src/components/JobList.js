2b26b2a54d44baae8f8a12dbcc9acbd93701c273
juanxsalazar <juanxsalazar21@gmail.com>
2019-04-16 15:58:06 -0400

    little bit more css

--- client/src/components/JobList.js
+++ client/src/components/JobList.js
@@ -1,48 +1,46 @@
-import React, { Component } from 'react';
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
 
 class JobList extends Component {
-   state = {
+  state = {
     jobs: []
-   }
+  };
 
-componentDidMount() {
-    this.loadJobs(this.props.id)
-}
+  componentDidMount() {
+    this.loadJobs(this.props.id);
+  }
 
-componentWillReceiveProps(newProps) {
-    this.loadJobs(newProps.id)
-}
+  componentWillReceiveProps(newProps) {
+    this.loadJobs(newProps.id);
+  }
 
-loadJobs(id) {
-    axios
-    .get(`/api/businesses/${id}/jobs`)
-    .then(response => {
-        this.setState({ jobs: response.data})
-    })
- 
-}
+  loadJobs(id) {
+    axios.get(`/api/businesses/${id}/jobs`).then(response => {
+      this.setState({ jobs: response.data });
+    });
+  }
 
-    render() {
-        if (this.state.jobs.length === 0) {
-            return <div className="small-font">Currently not hiring at this moment.</div>
-        }
-        return (
-            <ol className="small-font"> 
-                {
-                        this.state.jobs.map(job => <li>
-                      <b> Job: </b> {job.title}, {job.schedule_type}  <br></br>   
-                      <b> Posted Date: </b> {job.posted_date} <br></br>
-                      <b>  Pay Range: </b> {job.pay_range}      <br></br>
-                      <b> Requirements: </b> {job.requirements}   <br></br>     
-                      <b> Description: </b> {job.description}    <br></br>    
-                      <b> How to Apply: </b> {job.how_to_apply}         
-                        </li>)
-                    }
-        
-            </ol>
-        );
+  render() {
+    if (this.state.jobs.length === 0) {
+      return (
+        <div className="small-font">Currently not hiring at this moment.</div>
+      );
     }
+    return (
+      <ol className="small-font">
+        {this.state.jobs.map(job => (
+          <li>
+            <b> Job: </b> {job.title}, {job.schedule_type} <br />
+            <b> Posted Date: </b> {job.posted_date} <br />
+            <b> Pay Range: </b> {job.pay_range} <br />
+            <b> Requirements: </b> {job.requirements} <br />
+            <b> Description: </b> {job.description} <br />
+            <b> How to Apply: </b> {job.how_to_apply}
+          </li>
+        ))}
+      </ol>
+    );
+  }
 }
 
 export default JobList;
--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import MapGL, { Marker, NavigationControl, Popup } from "react-map-gl";
-import axios from 'axios'
-import ppl from '../images/ppl.png'
+import axios from "axios";
+import ppl from "../images/ppl.png";
 import cash from "../images/cash.png";
 import JobList from "./JobList";
 
@@ -12,7 +12,7 @@ class Map extends Component {
     this.state = {
       search: "",
       userLocation: null,
-      popupInfo: null, 
+      popupInfo: null,
       sideBarJobs: null,
       viewport: {
         latitude: 27.7700989,
@@ -26,14 +26,13 @@ class Map extends Component {
 
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(position => {
-      const { latitude, longitude } = position.coords
+      const { latitude, longitude } = position.coords;
 
       this.setState({
         userLocation: { lat: latitude, lng: longitude }
-      })
-    })
+      });
+    });
   }
-
 
   _updateViewport = viewport => {
     this.setState({ viewport });
@@ -47,22 +46,26 @@ class Map extends Component {
   };
 
   renderSideBarJobs = () => {
-    const { popupInfo } = this.state
-  
+    const { popupInfo } = this.state;
+
     if (!popupInfo) {
-      return <div>Please click on a marker to see job postings.</div>
+      return (
+        <div>
+          <h3>Please click on a marker to see job postings.</h3>
+        </div>
+      );
     }
-  
-    return <JobList id={popupInfo.id}/>
-  }
+
+    return <JobList id={popupInfo.id} />;
+  };
 
   renderPopup = () => {
-    const { popupInfo } = this.state
-  
+    const { popupInfo } = this.state;
+
     if (!popupInfo) {
-      return
+      return;
     }
-  
+
     return (
       <Popup
         tipSize={10}
@@ -71,101 +74,99 @@ class Map extends Component {
         latitude={popupInfo.latitude}
         closeOnClick={false}
         onClose={() => {
-          this.setState({ popupInfo: null })
+          this.setState({ popupInfo: null });
         }}
       >
         <div className="infobox">
           <p>{popupInfo.name}</p>
           <p>{popupInfo.address}</p>
         </div>
-  
       </Popup>
-    )
-  }
+    );
+  };
 
-  
-  searchChange = (event) => {
+  searchChange = event => {
     this.setState({
       search: event.target.value
-    })
-  }
+    });
+  };
 
-  doSearch = (event) =>{
-    axios.get(`/api/geocode/lookup?search=${this.state.search}`).then(response => {
-      this.setState({
-        userLocation: { lat: response.data[0], lng: response.data[1] },
-        viewport: {
-          latitude: response.data[0],
-          longitude: response.data[1],
-          zoom: 12.5,
-          bearing: 0,
-          pitch: 0
-        }
-      })
+  doSearch = event => {
+    axios
+      .get(`/api/geocode/lookup?search=${this.state.search}`)
+      .then(response => {
+        this.setState({
+          userLocation: { lat: response.data[0], lng: response.data[1] },
+          viewport: {
+            latitude: response.data[0],
+            longitude: response.data[1],
+            zoom: 12.5,
+            bearing: 0,
+            pitch: 0
+          }
+        });
+      });
+  };
 
-    })
-  }
-  
   render() {
     const { viewport } = this.state;
 
     return (
-    <>
-      <div className="center"> 
-      <input type="search" onChange={this.searchChange} value={this.search} placeholder="Search by Address or Zipcode">
-      </input>
-    <button onClick={this.doSearch} className="search">
-    Search
-    </button>  
-   </div>    
-    <div id="map" className="map" >
-      <div className="sidebar">
-
-        {this.renderSideBarJobs()}
-
-      </div> 
-        <MapGL
-          {...viewport}
-          width="100%"
-          height="100%"
-          mapStyle="mapbox://styles/mapbox/streets-v11"
-          mapboxApiAccessToken="pk.eyJ1IjoianVhbjIzc2FsYXphciIsImEiOiJjanUyaW0xMWIwY3QxNDRvN3ZnMW91N3BxIn0._YtrtrN7f2ba2F4S3HVL2Q"
-          onViewportChange={this._updateViewport}
-        > 
-          {this.renderPopup()}  
-        {this.props.businesses.map(business => (
-          <Marker
-            latitude={business.latitude}
-            longitude={business.longitude}
-            offsetTop={-32}
-            offsetLeft={-16}
+      <>
+        <div className="center">
+          <input
+            type="search"
+            onChange={this.searchChange}
+            value={this.search}
+            placeholder="Search by Address or Zipcode"
+          />
+          <button onClick={this.doSearch} className="search">
+            Search
+          </button>
+        </div>
+        <div id="map" className="map">
+          <div className="sidebar">{this.renderSideBarJobs()}</div>
+          <MapGL
+            {...viewport}
+            width="100%"
+            height="100%"
+            mapStyle="mapbox://styles/mapbox/streets-v11"
+            mapboxApiAccessToken="pk.eyJ1IjoianVhbjIzc2FsYXphciIsImEiOiJjanUyaW0xMWIwY3QxNDRvN3ZnMW91N3BxIn0._YtrtrN7f2ba2F4S3HVL2Q"
+            onViewportChange={this._updateViewport}
           >
-            <img 
-            width={32} 
-            height={32} 
-            src={cash}
-            onClick={() => {
-              this.setState({ popupInfo: business })
-            }} 
-            />
-          </Marker>
-))}
-{this.state.userLocation && <Marker 
-latitude={this.state.userLocation.lat}
-longitude={this.state.userLocation.lng}
-offsetTop={-64}
-offsetLeft={-32} > 
- <img 
-            width={64} 
-            height={64} 
-            src={ppl}/>
-
-</Marker>}
-          <div className="nav" style={this.navStyle}>
-            <NavigationControl onViewportChange={this._updateViewport} />
-          </div>
-        </MapGL>
-      </div>
+            {this.renderPopup()}
+            {this.props.businesses.map(business => (
+              <Marker
+                latitude={business.latitude}
+                longitude={business.longitude}
+                offsetTop={-32}
+                offsetLeft={-16}
+              >
+                <img
+                  width={32}
+                  height={32}
+                  src={cash}
+                  onClick={() => {
+                    this.setState({ popupInfo: business });
+                  }}
+                />
+              </Marker>
+            ))}
+            {this.state.userLocation && (
+              <Marker
+                latitude={this.state.userLocation.lat}
+                longitude={this.state.userLocation.lng}
+                offsetTop={-32}
+                offsetLeft={-16}
+              >
+                <img width={32} height={32} src={ppl} />
+              </Marker>
+            )}
+            <div className="nav" style={this.navStyle}>
+              <NavigationControl onViewportChange={this._updateViewport} />
+            </div>
+          </MapGL>
+        </div>
       </>
     );
   }
